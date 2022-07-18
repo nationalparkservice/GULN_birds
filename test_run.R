15f521f8233f6680267512ef29c0d2a2e67611af
Cheng <echeng@nps.gov>
2022-07-18 13:35:09 -0400

    Updated data check to use full data since 2010

--- test_run.R
+++ test_run.R
@@ -2,50 +2,32 @@
 # > Effects of date (day-of-year) and time?
 # > Different slopes by habitat type?
 
+rm(list = ls())
+
 ### Load packages ----
-library(sf)
 library(tidyverse)
 library(brms)
+library(partykit) # for regression trees
+library(detect)
+library(magrittr)
 
 ### Functions ----
-FuncDetectTable <- function(dat, colname, event_cov = TRUE) {
-  dat %>% 
-    dplyr::group_by(get(colname)) %>% 
-    dplyr::summarize(
-      num_locs = ifelse(event_cov, NA, length(unique(location_name))),
-      num_surveys = n(),
-      avg_detect = sum(sum_indiv)/num_surveys)
-}
-
-FuncDetectPlot <- function(spec_code, dat, colname) {
-  ggplot(dat, aes_string(x = colname, y = "sum_indiv")) +
-    geom_violin(scale = "count", fill = "lightblue", alpha = 0.5) +
-    stat_summary(fun = "mean", geom = "crossbar",  width = 0.75, fatten = 3, colour = "red") +
-    stat_summary(fun = "median", geom = "crossbar",  width = 0.75, fatten = 3, colour = "red", linetype = "dashed") +
-    geom_jitter(height = 0.2, width = 0.4, alpha = 0.25) +
-    labs(title = paste0(spec_code, " detections, by ", colname), y = "Detections", subtitle = "Each (jittered) point represents a point count survey.\nRed solid line shows average detections/survey; dashed line shows median.") +
-    theme_bw(base_size = 12)
-}
-
-FuncDetectTimePlot <- function(spec_code, dat, colname) {
-  ggplot(dat, aes_string(x = "yr_visit", y = "sum_indiv")) +
-    geom_violin(scale = "count", aes(fill = factor(yr)), alpha = 0.35) +
-    stat_summary(fun = "mean", geom = "crossbar",  width = 0.75, fatten = 2, colour = "red") +
-    stat_summary(fun = "median", geom = "crossbar",  width = 0.75, fatten = 2, colour = "red", linetype = "dashed") +
-    geom_jitter(height = 0.2, width = 0.4, alpha = 0.25) +
-    ggthemes::scale_fill_colorblind(name = "Year") +
-    labs(title = paste0(spec_code, " detections, by ", colname), y = "Detections", subtitle = "Each (jittered) point represents a point count survey.\nRed solid line shows average detections/survey; dashed line shows median.") +
-    theme_bw(base_size = 12) +
-    facet_wrap(vars(get(colname)))
-}
-
-FuncDetectCorrPlot <- function(spec_code, dat, colname) {
-  ggplot(dat, aes_string(x = colname, y = "sum_indiv")) +
-    geom_count() +
-    scale_size_area() +
-    geom_smooth() +
-    labs(title = paste0(spec_code, " detections, by ", colname), y = "Detections", subtitle = "Point size is scaled to number of point count surveys.") +
-    theme_bw(base_size = 12)
+FuncComb <- function(dat) {
+  dat %<>%
+    dplyr::mutate(yr_base = yr - min(yr, na.rm = TRUE),
+                  weather_wind_comb = case_when(
+                    weather_wind %in% c("0_calm", "1_smoke_drifts", "2_light_breeze") ~ "calm",
+                    weather_wind %in% c( "3_constant_breeze", "4_branches_move", "5_trees_sway") ~ "windy"
+                  ),
+                  weather_noise_comb = case_when(
+                    weather_noise == "0_low" ~ "low",
+                    weather_noise %in% c("1_moderate", "2_high") ~ "high"
+                  ),
+                  hab_type_comb = case_when(
+                    hab_type == "forest" ~ "forest",
+                    hab_type != "forest" ~ "not_forest"
+                  )
+    )
 }
 
 FuncLOOTable_BRM <- function(mod_list, AICwts_vec, stackwts_vec) {
@@ -81,59 +63,38 @@ return(loo_summary)
 df_locs <- readRDS(here::here("Data_out", "df_locs.RDS")) # site locations
 df_survey_conditions <- readRDS(here::here("Data_out", "df_survey_conditions.RDS")) # event covariate data
 df_full_obs <- readRDS(here::here("Data_out", "df_full_obs.RDS"))
+df_finaldat <- readRDS(here::here("Data_out", "df_finaldat.RDS"))
 
-### Subset the data ----
+### Subset the observation summary data ----
 tt<-df_full_obs %>% dplyr::filter(species_code == "TUTI") %>%
   dplyr::left_join(df_locs[c("location_name", "physiognomy", "hab_type")], by = "location_name") %>%
-  dplyr::left_join(df_survey_conditions[c("location_name", "event_date", "weather_wind", "weather_temperature", "weather_sky", "weather_noise")], by = c("location_name", "event_date"))
+  dplyr::left_join(df_survey_conditions[c("location_name", "event_date", "julian_prop", "start_time_interval", "weather_wind", "weather_wind_num", "weather_temperature", "weather_temperature_cs", "weather_sky", "weather_sky_revised_num", "weather_noise", "weather_noise_num")], by = c("location_name", "event_date"))
 
-### Summary tables and figures ----
+### Classification trees for exploring covariates ----
+tt_comb <- FuncComb(tt) 
+tt_tree <- partykit::ctree(sum_indiv ~ as.factor(unit_code) + as.factor(hab_type_comb) + weather_wind_num + weather_sky_revised_num + weather_temperature_cs + weather_noise_num + julian_prop, data = tt_comb)
 
-# Distribution of habitats across park units
-table(tt$unit_code, tt$physiognomy) # distribution of physiog across parks
-table(tt$unit_code, tt$hab_type) # distribution of hab_type across parks. BITH and GUIS-MS only have forest. GUIS-FL has the highest % of open.
-
-# Detection counts vs park unit
-FuncDetectTable(tt, "unit_code", event_cov = FALSE)
-FuncDetectPlot(spec_code = "TUTI", tt, colname = "unit_code")
-FuncDetectTimePlot(spec_code = "TUTI", tt, colname = "unit_code")
-
-# Examine detection counts vs point location or event covariates
-FuncDetectTable(tt, "physiognomy", event_cov = FALSE)
-FuncDetectPlot(spec_code = "TUTI", tt, colname = "physiognomy")
-
-FuncDetectTable(tt, "hab_type", event_cov = FALSE)
-FuncDetectPlot(spec_code = "TUTI", tt, colname = "hab_type")
-FuncDetectTimePlot(spec_code = "TUTI", tt, colname = "hab_type")
-
-FuncDetectTable(tt, "weather_wind")
-FuncDetectPlot(spec_code = "TUTI", tt, colname = "weather_wind")
-FuncDetectTable(tt, "weather_wind")
-
-FuncDetectTable(tt, "weather_sky")
-FuncDetectPlot(spec_code = "TUTI", tt, colname = "weather_sky")
-
-FuncDetectTable(tt, "weather_noise")
+plot(tt_tree)
+# table(predict(tt_tree), tt$sum_indiv)
+# unit_code (JELA has most detections) is the main predictor. Habitat type (combined) is next predictor. When habitat type was NOT combined, it did not come out as a predictor. When hab_type_comb is not a predictor, weather_noise is a predictor. Lowest mean detections is for noisy non-JELA surveys in non-forest habitats. From EDA plots, though, marsh (a non-forest habitat) habitats had detections of TUTI as high as forest plots 
+# No important predictors if each unit_code evaluated separately.
+# NOTE: Table of unit code vs noise shows that JELA (which had highest densities) also had lowest noise surveys, so they are somewhat confounded possibly
+# table(tt$unit_code, tt$weather_noise)
+# 
+ggplot(tt, aes(x = as.factor(yr), y = sum_indiv)) +
+  geom_boxplot() +
+  stat_summary(fun = "mean", geom = "crossbar",  width = 0.75, fatten = 3, colour = "red") +
+  stat_summary(fun = "median", geom = "crossbar",  width = 0.75, fatten = 3, colour = "red", linetype = "dashed") +
+  geom_jitter(height = 0.2, width = 0.4, alpha = 0.25) +
+  labs(title = "TUTI detections, by weather_noise and unit_code", y = "Detections", subtitle = "Each (jittered) point represents a point count survey.\nRed solid line shows average detections/survey; dashed line shows median.") +
+  theme_bw(base_size = 12) +
+  facet_wrap(vars(unit_code))
 
 ### BRMS ANALYSIS ----
 # Model data
-# Combine some levels for TUTI
-mod_dat <- tt %>%
-  dplyr::mutate(yr_base = yr - min(yr, na.rm = TRUE),
-weather_wind_comb = case_when(
-weather_wind %in% c("0_calm", "1_smoke_drifts") ~ "calm",
-weather_wind %in% c("2_light_breeze", "3_constant_breeze", "4_branches_move", "5_trees_sway") ~ "windy"
-),
-weather_noise_comb = case_when(
-weather_noise == "0_low" ~ "low",
-weather_noise %in% c("1_moderate", "2_high") ~ "high"
-),
-hab_type_comb = case_when(
-hab_type == "forest" ~ "forest",
-hab_type != "forest" ~ "not_forest"
-)
-) %>%
-dplyr::select(sum_indiv, yr_base, unit_code, location_name, hab_type_comb, weather_wind_comb, weather_noise_comb)
+# Combine some category levels into meaningful groups, for TUTI. I did also rerun tree classifcation with the combined covariates but still only unit_code was important.
+mod_dat <- tt_comb %>%
+  dplyr::select(sum_indiv, yr_base, unit_code, location_name, hab_type, hab_type_comb, weather_wind_num, weather_wind_comb, weather_noise_num, weather_noise_comb, julian_prop)
 
 # First, count the number of NA's in different columns b/c that will affect what records are dropped in different models. summary() doesn't count NA's in character columns, so use this instead:
 colSums(is.na(mod_dat))
@@ -153,36 +114,52 @@ m0_brm <- brm(sum_indiv ~ yr_base*unit_code + (1|location_name),
 m0_brm <- add_criterion(m0_brm, criterion = c("loo", "waic"), force_save = TRUE)
 
 # ... habitat type
-m1_brm <- update(m0_brm, formula. = ~ . + hab_type_comb)
+m1_brm <- update(m0_brm, formula. = ~ . + hab_type_comb, newdata = mod_dat_complete, file = here::here("Model_fits", "TUTI_m1"))
 m1_brm <- add_criterion(m1_brm, criterion = c("loo", "waic"), force_save = TRUE)
 
+# ... habitat type with PRIORS
+m1_priors_brm <- brm(sum_indiv ~ yr_base*unit_code + hab_type_comb + (1|location_name),
+                     data = mod_dat_complete,
+                     family = poisson,
+                     prior = c(prior(normal(0, 100), class = Intercept), 
+                               prior(normal(0, 1), class = b)), 
+                     iter = 4000, warmup = 1000, chains = 4, cores = 4,
+                     file = here::here("Model_fits", "TUTI_m1_priors_brm")
+)
+m1_priors_brm <- add_criterion(m1_priors_brm, criterion = c("loo", "waic"), force_save = TRUE)
+
+
 # ...noise
-m2_brm <- update(m0_brm, formula. = ~ . + weather_noise_comb)
+m2_brm <- update(m0_brm, formula. = ~ . + weather_noise_num, newdata = mod_dat_complete, file = here::here("Model_fits", "TUTI_m2"))
 m2_brm <- add_criterion(m2_brm, criterion = c("loo", "waic"), force_save = TRUE)
 
 # ...wind
-m3_brm <- update(m0_brm, formula. = ~ . + weather_wind_comb)
-m3_brm <- add_criterion(m3_brm, criterion = c("loo", "waic"), force_save = TRUE)
+m3_brm <- update(m0_brm, formula. = ~ . + weather_wind_comb, newdata = mod_dat_complete, file = here::here("Model_fits", "TUTI_m3"))
+m3_brm <- add_criterion(m3_brm, criterion = c("loo", "waic"), force_save = TRUE) # no divergence probs
 
 # ...wind and noise
-m4_brm <- update(m0_brm, formula. = ~ . + weather_wind_comb + weather_noise_comb)
+m4_brm <- update(m0_brm, formula. = ~ . + weather_wind_comb + weather_noise_num, newdata = mod_dat_complete, file = here::here("Model_fits", "TUTI_m4"))
 m4_brm <- add_criterion(m4_brm, criterion = c("loo", "waic"), force_save = TRUE)
 
 # ... habitat type and noise
-m5_brm <- update(m0_brm, formula. = ~ . + hab_type_comb + weather_noise_comb)
+m5_brm <- update(m0_brm, formula. = ~ . + hab_type_comb + weather_noise_num, newdata = mod_dat_complete, file = here::here("Model_fits", "TUTI_m5"))
 m5_brm <- add_criterion(m5_brm, criterion = c("loo", "waic"), force_save = TRUE)
 
 # ... habitat type and wind
-m6_brm <- update(m0_brm, formula. = ~ . + hab_type_comb + weather_wind_comb)
+m6_brm <- update(m0_brm, formula. = ~ . + hab_type_comb + weather_wind_comb, newdata = mod_dat_complete, file = here::here("Model_fits", "TUTI_m6"))
 m6_brm <- add_criterion(m6_brm, criterion = c("loo", "waic"), force_save = TRUE)
 
 # ... habitat type, wind and noise
-m7_brm <- update(m0_brm, formula. = ~ . + hab_type_comb + weather_wind_comb + weather_noise_comb)
+m7_brm <- update(m0_brm, formula. = ~ . + hab_type_comb + weather_wind_comb + weather_noise_num, newdata = mod_dat_complete,
+file = here::here("Model_fits", "TUTI_m7"))
 m7_brm <- add_criterion(m7_brm, criterion = c("loo", "waic"), force_save = TRUE)
 
+# ... julian date. Trying this one b/c best model includes habitat and second best includes habitat and noise.
+m8_brm <- update(m0_brm, formula. = ~ . + julian_prop, newdata = mod_dat_complete, file = here::here("Model_fits", "TUTI_m8"))
+m8_brm <- add_criterion(m8_brm, criterion = c("loo", "waic"), force_save = TRUE)
 
 ## Evaluate BRMS models
-mod = m4_brm 
+mod = m6_brm 
 summary(mod) # slightly shorter summary of model results
 
 # Coefficient plot
@@ -197,51 +174,38 @@ pp_check(mod, type = "dens_overlay")
 pp_check(mod, type = "stat_2d")
 pp_check(mod, type = "loo_pit_qq")
 pp_check(mod, type = "rootogram")
-pp_check(mod, type = "bars")
 pp_check(mod, type = "bars_grouped", group = "unit_code")
 pp_check(mod, type = "bars_grouped", group = "yr_base")
 pp_check(mod, type = "violin_grouped", group = "unit_code")
 
 ## Plot conditional effects
-plot(conditional_effects(mod, effects = "unit_code"), points=TRUE, point_args = list(width = 0.3, height = 0.1, alpha = 0.4))
+# plot(conditional_effects(mod, effects = "unit_code"), points=TRUE, point_args = list(width = 0.3, height = 0.1, alpha = 0.4))
 
 # Save conditional effects plots as list, for use with ggplot functions
 p <- plot(conditional_effects(mod), points=TRUE, point_args = list(width = 0.3, height = 0.1, alpha = 0.4), plot = FALSE)
 p$unit_code + theme_bw(base_size = 14)
-p$`yr_base:unit_code`
-p$`yr_base:unit_code` + facet+wrap(vars(unit_code))
-p$`yr_base:unit_code` + facet_wrap(vars(unit_code))
+p$`yr_base:unit_code` + theme_bw(base_size = 14) + facet_wrap(vars(unit_code))
 
 ## Look for high leverage points
 plot(loo(mod), label_points = TRUE)
 
 ## Compare BRMS models
 # To get a table with model weights (assumes you've added the IC to the model, as shown above)...
-mod_list <- list(m0_brm = m0_brm, m1_brm = m1_brm, m2_brm = m2_brm, m3_brm = m3_brm)
-AICwts_vec = model_weights(m0_brm, m1_brm, m2_brm, m3_brm, weights = "loo") # assumes you've used 'add_ic' to append LOO-IC to each model
-stackwts_vec = model_weights(m0_brm, m1_brm, m2_brm, m3_brm, weights = "stacking")
+mod_list <- list(m0_brm = m0_brm, m1_priors_brm = m1_priors_brm, m3_brm = m3_brm, m6_brm = m6_brm)
+AICwts_vec = model_weights(m0_brm, m1_priors_brm, m3_brm, m6_brm,  weights = "loo") # assumes you've used 'add_ic' to append LOO-IC to each model
+stackwts_vec = model_weights(m0_brm, m1_priors_brm, m3_brm, m6_brm, weights = "stacking")
+
+# mod_list <- list(m0_brm = m0_brm, m1_brm = m1_brm, m1_priors_brm = m1_priors_brm, m2_brm = m2_brm, m3_brm = m3_brm, m4_brm = m4_brm, m5_brm = m5_brm, m6_brm = m6_brm, m7_brm = m7_brm, m8_brm = m8_brm)
+# AICwts_vec = model_weights(m0_brm, m1_brm, m1_priors_brm, m2_brm, m3_brm, m4_brm, m5_brm, m6_brm, m7_brm, m8_brm,  weights = "loo") # assumes you've used 'add_ic' to append LOO-IC to each model
+# stackwts_vec = model_weights(m0_brm, m1_brm, m1_priors_brm, m2_brm, m3_brm, m4_brm, m5_brm, m6_brm, m7_brm, m8_brm, weights = "stacking")
+
 FuncLOOTable_BRM(mod_list = mod_list, AICwts_vec = AICwts_vec, stackwts_vec = stackwts_vec)
 
 ## BRMS UPSHOT
-# Had problems w/model when I used the original covariates
-# interesting! once I included unit_code, the noise effect was not significant. table of unit code vs noise shows that JELA (which had highest densities) also had lowest noise surveys. So should plot out noise effect by unit code separately.
-# BUT...NOTE THE RANDOM EFFECT DISTRIBUTION HIGHLY NON-NORMAL, PROBABLY NEED TO FIX SOMETHING WITH THAT...
-table(tt$unit_code, tt$weather_noise)
+# Best-supported model included hab_type_comb (forest vs. non-forest), next supported also added noise but it's far less supported. This is consistent with classification tree results
+# <<<<<<<<<<<<<< PICK UP FROM HERE--BRMS PERFORMS BETTER WITH THE PRIORS, BUT NEED TO CENTER/SCALE EVERYTHING AND SELECT WEAKLY INFORMATIVE PRIORS. INCLUDE TEMPERATURE AS A POLYNOMIAL.
 
-ggplot(tt, aes_string(x = "weather_noise", y = "sum_indiv", fill = "unit_code")) +
-  geom_violin(scale = "count", alpha = 0.5) +
-  stat_summary(fun = "mean", geom = "crossbar",  width = 0.75, fatten = 3, colour = "red") +
-  stat_summary(fun = "median", geom = "crossbar",  width = 0.75, fatten = 3, colour = "red", linetype = "dashed") +
-  geom_jitter(height = 0.2, width = 0.4, alpha = 0.25) +
-  labs(title = "TUTI detections, by weather_noise and unit_code", y = "Detections", subtitle = "Each (jittered) point represents a point count survey.\nRed solid line shows average detections/survey; dashed line shows median.") +
-  theme_bw(base_size = 12) + 
-  facet_wrap(vars(unit_code))
-
-
-
-
-
-# # Examine data on habitat maps
+# # Examine data on habitat maps ----
 # # THese have physiognomy: GUIS
 # # SAAN doesn't even have a veg polygon shapefile
 # (map_layers <- st_layers(here::here("Data_in", "guisgeodata.gdb")))
@@ -253,3 +217,41 @@ ggplot(tt, aes_string(x = "weather_noise", y = "sum_indiv", fill = "unit_code"))
 # leaflet() %>% 
 #   addTiles() %>%
 #   addPolygons(data = map_veg, color = "black", weight = 1.25, fillColor = ~pal(Physiognomy), fillOpacity = 0.5)
+
+### SOLYMOS METHOD ----
+tt_raw <- df_finaldat %>% dplyr::filter(species_code == "TUTI") %>%
+  dplyr::left_join(df_locs[c("location_name", "physiognomy", "hab_type")], by = "location_name") %>%
+  dplyr::left_join(df_survey_conditions[c("location_name", "event_date", "weather_wind", "weather_temperature_cs", "weather_sky_revised_num", "weather_noise_num", "weather_noise", "julian_prop", "start_time_interval")], by = c("location_name", "event_date"))
+tt_raw_comb <- FuncComb(tt_raw)
+
+# These are the distance classes
+unique(tt_raw[c("distance_bin", "distance_bin_id")])
+
+## Testing assumptions
+# >>>>> REALLY, TIME INTERVAL SHOULD BE RELATIVE TO SUNRISE <<<<
+# >>>>> TEST EFFECT OF JULIAN DAY <<<<<
+
+# ASSUMPTION: Half-normal distribution for distance
+
+# Plot of actual distribution
+ggplot(tt_raw, aes(x= distance_bin_id, weight = count)) + geom_bar() # lower detections in closest distance bin BUT this may be appropriate, considering that the closest bins have smaller area. See QPAD book.
+
+# Does it differ by habitat type?
+ggplot(tt_raw, aes(x= distance_bin_id, weight = count)) + geom_bar() + facet_grid(cols = vars(hab_type)) # the more open the habitat, the less likely birds detected at closer distance classes (but sample sizes are small)
+
+# Does it differ by time interval?
+ggplot(tt_raw, aes(x= distance_bin_id, weight = count)) + geom_bar() + facet_grid(cols = vars(time_bin)) # it seems that the later time periods, observers more likely to record the birds from farther distance classes (perhaps the first birds they detect/record tend to be the ones closer in, then as those are "removed" they detect the ones farther out?)
+
+
+# ASSUMPTION: Exponential distribution for time since first detection
+
+# Plot of actual distribution
+ggplot(tt_raw, aes(x= time_bin_id, weight = count)) + geom_bar()
+# it's really not exponential, perhaps b/c as time goes on it's hard to keep track of which birds have already been counted and also some birds may come into the survey area (immigration, a violation of closure assumption)
+
+# Does it differ by habitat type? Not enough non-forest data
+ggplot(tt_raw, aes(x= time_bin_id, weight = count)) + geom_bar() + facet_grid(cols = vars(hab_type)) 
+
+# Does it differ by noise?
+ggplot(tt_raw, aes(x= time_bin_id, weight = count)) + geom_bar() + facet_grid(cols = vars(weather_noise)) 
+
